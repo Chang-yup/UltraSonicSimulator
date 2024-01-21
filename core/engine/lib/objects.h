ca4a2ee8cdb3e026a7ca5c49e87bf274054c78ca
WOO <97onggi@gmail.com>
2024-01-22 00:11:06 +0900

    GUI(Controller) Added, File Structure Updated.

---
+++ core/engine/lib/objects.h
@@ -0,0 +1,16 @@
+class Object
+{
+    double centerPositionX;
+    double centerPositionY;
+    double soundAbsorptionCoefficient;
+}
+
+class Circle : public Object
+{
+    double radius;
+}
+
+class Rectangle : public Object
+{
+    double 
+}
